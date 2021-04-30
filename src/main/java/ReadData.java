59cc4d69767d8802602cf81d9af81b8aa3f7f90d
Badar Munir <badar.munir@esure.com>
2021-04-30 19:25:58 +0100

    convert to integer

--- src/main/java/ReadData.java
+++ src/main/java/ReadData.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadData {
     public static void main(final String[] args) {
@@ -8,8 +11,13 @@ public class ReadData {
     }
 
     private static void readFile() {
+        final List<Integer> vertices = new ArrayList<>();
         try (var reader = new BufferedReader(new FileReader("src/main/resources/kruskal-test1.txt"))) {
-            reader.lines().forEach(System.out::println);
+            final var verticesArray = reader.readLine().split(",");
+            for (String number : verticesArray) {
+                vertices.add(Integer.valueOf(number));
+            }
+            vertices.forEach(System.out::println);
         } catch (IOException e) {
             System.err.println("File Read Error");
         }
